5649a64cacbac0dcfc167ba9e98c9813d764a200
Lebang <lebangnong@gmail.com>
2023-08-26 17:23:47 +0200

    Nodejs server request and response

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const events = require('events');
+const http = require('http');
+
 
 let myEmitter = new events.EventEmitter();
 
@@ -7,3 +9,24 @@ myEmitter.on('someEvent', (mssg) => {
 });
 
 myEmitter.emit('someEvent', 'the event was emitted');
+
+const server = http.createServer((req,res) => {
+    if (req.url === '/') {
+        res.write('Hello World');
+        res.end();
+    }
+
+    if (req.url === '/api/courses') {
+        res.write(JSON.stringify([1, 2, 3]));
+        res.end();
+    }
+});
+
+/*
+server.on('connection', (socket) => {
+    console.log('new connection');
+}) */
+
+server.listen(4000);
+
+console.log('Listening on port 4000...');
